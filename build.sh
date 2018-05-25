734bdbefa5099dd5904cb748e45bc1a9c33aa927
Layne Penney <layne@evove.tech>
2018-05-25 15:43:01 -0500

    update again

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+source ${dir}/hooks/__env.sh
 ( cd ${dir}/hooks && ./build )
--- push.sh
+++ push.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${dir}/hooks/__env.sh
 
 ( cd ${dir}/hooks && ./build )
-
 docker push "${DOCKER_REPO}:${CACHE_TAG}"
-
 ( cd ${dir}/hooks && ./post_push )
