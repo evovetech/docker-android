2d7dc97c3fcaf0fdd16c680895ffe1a7bc7cce4e
Layne Penney <layne@evove.tech>
2018-07-20 01:40:30 -0500

    update for ci

--- env.sh
+++ env.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-export ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+default_gradle_version="gradle-4.5.1"
 
-if [[ -z "${DOCKERFILE_PATH}" ]]; then
-  root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  export DOCKERFILE_PATH="${root}/Dockerfile"
+function defaultCacheTag() {
+    if [[ -z "${CACHE_TAG}" ]]; then
+      printf "${default_gradle_version}"
+    else
+      printf "${CACHE_TAG}"
+    fi
+}
+
+if [[ -z "${GRADLE_VERSION}" ]]; then
+    local tag="$( defaultCacheTag )"
+    echo "defaultCacheTag=${tag}"
+    case $tag in
+      gradle- )
+        ## ignore
+        ;;
+      * )
+        tag="${default_gradle_version}"
+        ;;
+    esac
+    echo "cacheTag=${tag}"
+    export GRADLE_VERSION="${tag}"
 fi
 
-default_gradle_version="gradle-4.5.1"
-if [[ -z "${GRADLE_VERSION}" ]]; then
-  export GRADLE_VERSION="${default_gradle_version}"
+if [[ -z "${CACHE_TAG}" ]]; then
+  export CACHE_TAG="${GRADLE_VERSION}"
+}
+
+if [[ -z "${DOCKERFILE_PATH}" ]]; then
+  export DOCKERFILE_PATH="${ROOT_DIR}/Dockerfile"
 fi
