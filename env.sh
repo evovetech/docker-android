3a34ac413cfa9473ddc099ef5596e7b7fe76d063
Layne Penney <layne@evove.tech>
2018-07-20 12:30:08 -0500

    use buildpath

--- env.sh
+++ env.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-default_gradle_version="gradle-4.9"
+function main() {
+  local default_gradle_version="gradle-4.9"
+
+  if [[ -z "${DOCKER_TAG}" ]]; then
+    setGradleVersion
+    export DOCKER_TAG="${GRADLE_VERSION}"
+  else
+    setGradleVersion
+  fi
+
+  if [[ -z "${BUILD_PATH}" ]]; then
+    export BUILD_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  fi
+
+  if [[ -z "${DOCKER_REPO}" ]]; then
+    export DOCKER_REPO="evovetech/android"
+  fi
+
+  if [[ -z "${DOCKERFILE_PATH}" ]]; then
+    export DOCKERFILE_PATH="${BUILD_PATH}/Dockerfile"
+  fi
+
+  if [[ -z "${IMAGE_NAME}" ]]; then
+    export IMAGE_NAME="${DOCKER_REPO}:${DOCKER_TAG}"
+  fi
+}
 
 function defaultDockerTag() {
     if [[ -z "${DOCKER_TAG}" ]]; then
@@ -27,22 +52,5 @@ function setGradleVersion() {
   fi
 }
 
-if [[ -z "${DOCKER_REPO}" ]]; then
-  export DOCKER_REPO="evovetech/android"
-fi
-
-if [[ -z "${DOCKERFILE_PATH}" ]]; then
-  root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  export DOCKERFILE_PATH="${root}/Dockerfile"
-fi
-
-if [[ -z "${DOCKER_TAG}" ]]; then
-  setGradleVersion
-  export DOCKER_TAG="${GRADLE_VERSION}"
-else
-  setGradleVersion
-fi
-
-if [[ -z "${IMAGE_NAME}" ]]; then
-  export IMAGE_NAME="${DOCKER_REPO}:${DOCKER_TAG}"
-fi
+# RUN
+main "$@"
