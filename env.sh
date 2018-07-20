52b59e27cf2ea24dc924aef4abfadff8eab70b6b
Layne Penney <layne@evove.tech>
2018-07-20 12:21:11 -0500

    fix auto builds

--- env.sh
+++ env.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-echo '  $ printenv :'
-printenv
-echo ''
-echo ''
-
 default_gradle_version="gradle-4.9"
 
-function defaultCacheTag() {
-    if [[ -z "${CACHE_TAG}" ]]; then
+function defaultDockerTag() {
+    if [[ -z "${DOCKER_TAG}" ]]; then
       printf "${default_gradle_version}"
     else
-      printf "${CACHE_TAG}"
+      printf "${DOCKER_TAG}"
     fi
 }
 
 function setGradleVersion() {
   if [[ -z "${GRADLE_VERSION}" ]]; then
-      local tag="$( defaultCacheTag )"
-      echo "defaultCacheTag=${tag}"
+      local tag="$( defaultDockerTag )"
+      echo "defaultDockerTag=${tag}"
       case $tag in
         gradle- )
           ## ignore
@@ -27,27 +22,27 @@ function setGradleVersion() {
           tag="${default_gradle_version}"
           ;;
       esac
-      echo "cacheTag=${tag}"
+      echo "dockerTag=${tag}"
       export GRADLE_VERSION="${tag}"
   fi
 }
+
+if [[ -z "${DOCKER_REPO}" ]]; then
+  export DOCKER_REPO="evovetech/android"
+fi
 
 if [[ -z "${DOCKERFILE_PATH}" ]]; then
   root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   export DOCKERFILE_PATH="${root}/Dockerfile"
 fi
 
-if [[ -z "${CACHE_TAG}" ]]; then
+if [[ -z "${DOCKER_TAG}" ]]; then
   setGradleVersion
-  export CACHE_TAG="${GRADLE_VERSION}"
+  export DOCKER_TAG="${GRADLE_VERSION}"
 else
   setGradleVersion
 fi
 
-if [[ -z "${DOCKER_REPO}" ]]; then
-  export DOCKER_REPO="evovetech/android"
-fi
-
 if [[ -z "${IMAGE_NAME}" ]]; then
-  export IMAGE_NAME="${DOCKER_REPO}:${CACHE_TAG}"
+  export IMAGE_NAME="${DOCKER_REPO}:${DOCKER_TAG}"
 fi
